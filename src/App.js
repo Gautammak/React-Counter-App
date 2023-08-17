152ec887782e1433fc510dd91089171238adc72c
Gautammak <gautammakavana2001@gmail.com>
2023-08-17 13:53:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+//project -1
+// import React from 'react'
+// import Resturant  from './component/Basics/Resturant';
+
+
+// const App = () => {
+//   return < Resturant  />
+// };
+// export default App;
+
+
+import React from "react";
+// import Resturant from "./component/Basics/Resturant";cd 
+// import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+import UseReducer from "./component/Hooks/useReducer";
+const App = () => {
+  return <UseReducer />;
+};
+
+export default App;
